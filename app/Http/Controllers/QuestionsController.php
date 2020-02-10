4ef65e54f9849616f1f16bb6445234500ac09276
Daniel Rennie - Admin <DRAdmin@proclaims.loc>
2020-02-10 14:52:22 +0000

    added form validation and saved first question

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AskQuestionRequest;
 use App\Question;
 
 class QuestionsController extends Controller
@@ -37,8 +38,11 @@ class QuestionsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
+        $request->user()->questions()->create($request->only('title','body'));
+
+        return redirect()->route('questions.index')->with('success', 'your question has been submitted');
         
     }
 
