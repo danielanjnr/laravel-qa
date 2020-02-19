db1ebf66e3b4bdf3948e7f958fe80eb113c17c28
daniel <danielrennie@proclaim-care.co.uk>
2020-02-19 10:36:02 +0000

    added show question detail with slug using route model binding in routeserviceprovider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
+use App\Question;
+
 class RouteServiceProvider extends ServiceProvider
 {
     /**
@@ -23,7 +25,12 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //add binding for question slug in order to use route model binding on url
+        Route::bind('slug', function($slug){
+            $question = Question::where('slug', $slug)->first();
+
+            return $question ?? abort(404);
+        });
 
         parent::boot();
     }
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,25 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">All questions</div>
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h1>{{ $question->title }}</h1>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.index') }}">All questions</a>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="card-body">
-                   @foreach($questions as $question)
-                        <div class="media">
-                            <div class="media-body">
-                                <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                <p class="lead">
-                                    Asked by <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
-                                    <small class="text-muted">{{ $question->created_date }}</small>
-                                </p>
-                                {{ str_limit($question->body, 250) }}
-                            </div>
-                        </div>
-                        <hr>
-                   @endforeach
-                    <div class="mx-auto">
-                   {{ $questions->links() }}
-                   </div>
+                {!! $question->body_html !!}
                 </div>
             </div>
         </div>
